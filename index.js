774c35eb3f8d847037d54c022fb01ed6842349fd
recitativonika <fool-body-cheddar@duck.com>
2024-12-04 03:29:50 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+const { ethers } = require('ethers');
+const fs = require('fs');
+
+const COLORS = {
+  BOLD_YELLOW: '\x1b[1;33m',
+  CYAN: '\x1b[36m',
+  GREEN: '\x1b[32m',
+  RESET: '\x1b[0m'
+};
+
+function centerAlignText(text, width) {
+  const pad = Math.floor((width - text.length) / 2);
+  return ' '.repeat(pad) + text + ' '.repeat(pad);
+}
+
+const consoleWidth = process.stdout.columns;
+
+console.log("");
+console.log(`${COLORS.BOLD_YELLOW}${centerAlignText("============================================", consoleWidth)}${COLORS.RESET}`);
+console.log(`${COLORS.BOLD_YELLOW}${centerAlignText("Unstoppable Domain subdomain minter", consoleWidth)}${COLORS.RESET}`);
+console.log(`${COLORS.BOLD_YELLOW}${centerAlignText("github.com/recitativonika", consoleWidth)}${COLORS.RESET}`);
+console.log(`${COLORS.BOLD_YELLOW}${centerAlignText("============================================", consoleWidth)}${COLORS.RESET}`);
+console.log("");
+
+const privateKey = fs.readFileSync('priv.txt', 'utf8').trim();
+
+const provider = new ethers.JsonRpcProvider('https://polygon-rpc.com');
+
+const wallet = new ethers.Wallet(privateKey, provider);
+
+const contractAddress = '0x7be83293BeeDc9Eba1bd76c66A65F10F3efaeC26';
+const contractABI = [
+  {
+    "constant": false,
+    "inputs": [
+      { "name": "address", "type": "address" },
+      { "name": "subdomains", "type": "string[]" },
+      { "name": "records1", "type": "string[]" },
+      { "name": "records2", "type": "string[]" },
+      { "name": "flag", "type": "bool" }
+    ],
+    "name": "issueWithRecords",
+    "outputs": [],
+    "type": "function"
+  }
+];
+const contract = new ethers.Contract(contractAddress, contractABI, wallet);
+
+async function issueWithRecords() {
+  const address = "0x9052E8f0607736cB0085f5057cdCB1C783277040";
+
+  const mainDomain = fs.readFileSync('maindomain.txt', 'utf8').trim();
+  const domainParts = mainDomain.split('.');
+
+  if (domainParts.length < 2) {
+    console.error('Invalid domain format in maindomain.txt');
+    return;
+  }
+
+  const subContent = fs.readFileSync('sub.txt', 'utf8').trim();
+  const subdomainsFromFile = subContent.split('\n').map(sub => sub.trim()).filter(sub => sub);
+
+  if (subdomainsFromFile.length === 0) {
+    console.error('No subdomains found in sub.txt');
+    return;
+  }
+
+  const records1 = [];
+  const records2 = [];
+  const flag = false;
+
+  for (const subdomain of subdomainsFromFile) {
+    const subdomains = [subdomain, ...domainParts];
+    console.log(`Attempting to mint subdomain: ${COLORS.CYAN}${subdomain}.${mainDomain}${COLORS.RESET}`);
+    try {
+      const tx = await contract.issueWithRecords(address, subdomains, records1, records2, flag);
+      const receipt = await tx.wait();
+      console.log(`${COLORS.GREEN}Successfully issued domain with records for subdomain: ${subdomain}${COLORS.RESET}`);
+      console.log(`${COLORS.GREEN}Transaction hash: https://polygonscan.com/tx/${receipt.hash}${COLORS.RESET}`);
+    } catch (error) {
+      const reason = error.reason || 'Unknown error';
+      console.error(`Failed to issue domain with records for subdomain: ${subdomain}. Reason: ${reason}`);
+    }
+  }
+}
+
+issueWithRecords();
---
+++ readme.md
@@ -0,0 +1,57 @@
+# Unstoppable Domain subdomain minter
+
+Script designed to mass mint subdomains from your main domain (Unstoppable Domain) on the Polygon network.
+
+## Features
+
+- **mass mint subdomains**
+
+## Prerequisites
+
+- [Node.js](https://nodejs.org/) (version 12 or higher)
+- A funded account on the Polygon network.
+
+## Setup
+
+1. Clone the repository to your local machine:
+   ```bash
+   git clone https://github.com/recitativonika/ud-sub-minter.git
+   ```
+2. Navigate to the project directory:
+   ```bash
+   cd ud-sub-minter
+   ```
+3. Install the necessary dependencies:
+   ```bash
+   npm install
+   ```
+
+## Usage
+
+1. Prepare configuration files:
+    - edit `priv.txt` with your private key
+        ```
+        yourprivkey
+        ```
+    - edit `maindomain.txt` with your main Unstoppable Domain
+        ```
+        example.domain
+        ```
+    - edit `sub.txt` with list of subname of your domain that you want to mint, for example if you want to mint `sub1.example.domain` put `sub1` in the sub.txt
+        ```
+        subname1
+        subname2
+        subname3
+        ```
+      For above example, the script will mint `subname1.example.domain` `subname2.example.domain` and `subname3.example.domain`
+
+2. run the script
+    ```bash
+   node index.js
+    ```
+
+## License
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more details.
+
+## Note
+This script is for testing purposes only, I am not responsible for anything that happens with your account/wallet.
